6eabee19ec16f27b09452f06edb674af259a65fd
Yo Yehudi <yochannah@gmail.com>
2016-11-24 14:25:59 +0000

    bump version and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject venantius/accountant "0.1.7"
-  :description "Navigation for Single-Page Applications Made Easy."
-  :url "http://github.com/venantius/accountant"
+(defproject intermine/accountant-fragments "0.1.8"
+  :description "Navigation for Single-Page Applications Made Easy. Now with Fragments."
+  :url "https://github.com/intermine/accountant-fragments"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
